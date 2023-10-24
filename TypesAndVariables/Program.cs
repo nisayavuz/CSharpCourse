922c731180d163dc4a5431d66f0fc479815966d2
Nisa Yavuz <nsnryvz@outlook.com>
2023-10-24 15:21:07 +0300

    types and variables

---
+++ TypesAndVariables/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TypesAndVariables
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Value types
+            //Console.WriteLine("Hello World!");
+            double number5 = 10.4;
+            decimal number6 = 10.4m;
+            char character = 'a';
+            bool condition = false;
+            byte number4 = 255;
+            short number3 = 32767;
+            int number1 = 2147483647;
+            long number2 = 21474836486545646;
+            var number7 = 10;
+            number7 = 'A';
+            Console.WriteLine("Number1 is {0}", number1);
+            Console.WriteLine("Number2 is {0}", number2);
+            Console.WriteLine("Number 3 is {0}", number3);
+            Console.WriteLine("Number 4 is {0}", number4);
+            Console.WriteLine("Number 5 is {0}" , number5);
+            Console.WriteLine("Number 6 is {0}" , number6);
+            Console.WriteLine("Number 7 is {0}", number7);
+            Console.WriteLine("Character is {0}", (int)character);
+            Console.WriteLine((int)Days.Friday);
+            Console.ReadLine();
+        }
+    }
+
+    enum Days
+    {
+        Monday=10, Tuesday, Wednesday, Thursday, Friday, Saturday, Sunday
+    }
+}
