f5da1c75189bc7299bd2e4db78f1297b0e55246a
Nisa Yavuz <nsnryvz@outlook.com>
2023-10-26 16:35:18 +0300

    classes

---
+++ Classes/Customer.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Classes
+{
+    internal class Customer
+    {
+        //field tanımlamak:
+        // public string FirstName;
+
+        //property
+        string _firstName;
+        public int Id { get; set; }
+        public string FirstName { 
+            
+            get { return "Mrs." + _firstName; }
+            set { _firstName = value; }
+        }
+
+        public string LastName { get; set; }
+        public string City { get; set; }
+    }
+}
---
+++ Classes/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Classes
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            CustomerManager customerManager = new CustomerManager();
+            customerManager.Add();
+            customerManager.Update();
+
+            ProductManager productManager = new ProductManager();
+            productManager.Add();
+            productManager.Update();
+
+            Customer customer = new Customer();
+            customer.City = "Ankara";
+            customer.Id = 1;    
+            customer.FirstName = "Nisa";
+            customer.LastName = "Yavuz";
+
+            Customer customer2 = new Customer()
+            {
+                FirstName = "Nisa", Id = 2, LastName = "Yavuz" 
+
+            };
+
+            Console.WriteLine(customer2.FirstName);
+            Console.ReadLine();
+        }
+    }
+
+    
+}
